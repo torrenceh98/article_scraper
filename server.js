c7b664579b8553728ad1664bb08ddd41b4e5ee7b
Torrenceh98 <torrenceh98@gmail.com>
2018-11-03 17:32:10 -0500

    push

--- server.js
+++ server.js
@@ -170,6 +170,9 @@ app.post("/articles/:id", function(req, res) {
 });
 
 
-app.listen(process.env.PORT || 5000), function() {
-  console.log("App running on port " + PORT + "!");
-};
+// app.listen(process.env.PORT || 5000), function() {
+//   console.log("App running on port " + PORT + "!");
+// };
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
