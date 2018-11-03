470b12361155d318af6fb9c0fede5e733256b66b
Torrenceh98 <torrenceh98@gmail.com>
2018-11-03 17:23:14 -0500

    push

--- server.js
+++ server.js
@@ -2,57 +2,36 @@
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
-var PORT = 3001;
-
-// Initialize Express
+var PORT = 3000;
 var app = express();
+serveStatic = require('serve-static');
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
-
 
 // Handlebars
 var handlebars = require("handlebars");
-
 var exphbs = require("express-handlebars");
-//sets main.handlebars as the default layout and our view engine as handlebar
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars")
-
 handlebars.registerHelper("json", context => JSON.stringify(context));
-
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-
 // Routes
-
 app.get("/scrape", function(req, res) {
-
-    axios.get("https://www.nytimes.com/section/sports/basketball").then(function(response) {
-
-    var $ = cheerio.load(response.data);
   
+    axios.get("https://www.nytimes.com/section/sports/basketball").then(function(response) {
+    var $ = cheerio.load(response.data);
       $(".story-body").each(function(i, element) {
       var result = []
 
@@ -64,7 +43,6 @@ app.get("/scrape", function(req, res) {
     //  console.log(result.body);
     //  console.log(result.link)
     // console.log(result)
-
       db.Article.create(result)
         .then(function(dbArticle) {
           // console.log(dbArticle)
@@ -169,7 +147,11 @@ app.post("/articles/:id", function(req, res) {
     });
 });
 
+// app.use(serveStatic("../angularjs"));
 
 app.listen(process.env.PORT || 5000), function() {
   console.log("App running on port " + PORT + "!");
 };
+
+// app.listen(8180);
+// app.listen(5000);
